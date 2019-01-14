80e05c34ea06ed73b91886b9a2875435c1beb663
hulin12138 <hulin@pku.edu.cn>
2019-01-15 04:16:01 +0800

    add explore

--- web/urls.py
+++ web/urls.py
@@ -41,5 +41,7 @@ urlpatterns = [
     path('display_profile',control.edit_profile, name='display_profile'),
     path('update_profile',control.update_profile, name='update_profile'),
     path('change_passwd',control.change_passwd, name='change_passwd'),
-    path('enter_change_passwd',view.enter_change_passwd, name='enter_change_passwd')
+    path('enter_change_passwd',view.enter_change_passwd, name='enter_change_passwd'),
+    path('explore'.control.random_choose_user, name='explore'),
+    path('follow_in_explore',control.follow_in_explore, name='follow_in_explore')
 ]
